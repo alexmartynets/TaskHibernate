8437731ef99c6718868162d65db4c2c0c54459bc
Alexandr Martynets <amartinets@yandex.ru>
2020-07-13 15:38:05 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,7 +5,6 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
         final UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
